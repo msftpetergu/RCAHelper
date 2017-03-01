f9af790e0c42ad994306dfd0250c6e9b3f790c0e
Peter Gu <petergu@microsoft.com>
2017-03-01 04:07:42 +0000

    Generating string with .replace() instead of substring(indexof()).

---
+++ PS_Get-PublicRCA/PS_Get-PublicRCA/Get-PublicRCA.ps1
@@ -0,0 +1,12 @@
+﻿#Author: Peter Gu (petergu@microsoft.com)
+#Version: 1.0
+
+Param([Parameter(Mandatory=$True,Position=1)][String]$URL)
+#Original format: https://icm.ad.msft.net/portal/publicpostmortem/Details/450
+#Dest. format: https://icm.ad.msft.net/imp/PublicPostmortems.aspx?id=450
+
+$URL = $URL.Replace('https://icm.ad.msft.net/portal/publicpostmortem/Details/', 'https://icm.ad.msft.net/imp/PublicPostmortems.aspx?id=')
+
+$IE = New-Object -ComObject internetexplorer.application
+$IE.navigate2($URL)
+$IE.visible = $true
